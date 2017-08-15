ad3528380753a2ed8b464f9bb2dc5c9e89244410
saumya <2saumyaray@gmail.com>
2017-08-15 18:16:02 +0530

    wip: code organisation

---
+++ myApp/App/MyClassA.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+* A sample Class
+*/
+//class ClassName extends AnotherClass
+class ClassName
+{
+	public $var = 'a default value';
+	function __construct(argument)
+	{
+		//
+	}
+	public function displayVar() {
+        echo $this->var;
+    }
+}
+
+?>
